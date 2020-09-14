a460f4d40cca8a05566526cced056e1541469ced
maxwelsdk <maxwel.sdk@hotmail.com>
2020-09-14 09:13:18 -0300

    Spring annotations
    Declaração das anotações de spring, "tipagem" auxilia na configurações de debug

--- src/main/java/br/com/kommando/user/api/UserEndpoint.java
+++ src/main/java/br/com/kommando/user/api/UserEndpoint.java
@@ -5,13 +5,11 @@ import br.com.kommando.user.data.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
+@RestController
 @RequestMapping("users")
 public class UserEndpoint {
 
--- src/main/java/br/com/kommando/user/data/services/UserService.java
+++ src/main/java/br/com/kommando/user/data/services/UserService.java
@@ -3,9 +3,11 @@ package br.com.kommando.user.data.services;
 import br.com.kommando.user.data.models.User;
 import br.com.kommando.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserService {
 
     @Autowired
@@ -16,6 +18,7 @@ public class UserService {
     }
 
     public User user(User user) {
+        System.out.println("Ai baaaaaaa");
         return repository.save(user);
     }
 }
