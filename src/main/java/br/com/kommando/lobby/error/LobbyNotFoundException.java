cc569d67ec393da9d01ee1df2a89ba0f37dc7386
maxwelsdk <maxwel.sdk@hotmail.com>
2020-09-27 14:23:19 -0300

    Exception handling

--- src/main/java/br/com/kommando/lobby/error/LobbyNotFoundException.java
+++ src/main/java/br/com/kommando/lobby/error/LobbyNotFoundException.java
@@ -1,9 +1,5 @@
 package br.com.kommando.lobby.error;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class LobbyNotFoundException extends RuntimeException {
     public LobbyNotFoundException(String message) {
         super(message);
